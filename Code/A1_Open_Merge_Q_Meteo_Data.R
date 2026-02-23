bb813ecf4427a47db97532a6af1716200405e1ba
AudreyCampeau <audrey.campeau@umontreal.ca>
2026-02-23 16:58:15 -0500

    correct q spec

--- Code/A1_Open_Merge_Q_Meteo_Data.R
+++ Code/A1_Open_Merge_Q_Meteo_Data.R
@@ -14,13 +14,12 @@ library(tidyverse)
 #Data Source: Johannes Tiwari (Sent by Alberto in April 2024)
 #Q units are most likely L s-1
 # Q units can't be L/s, they are too high!! 
-Q_DC2=read.csv("Input/Q and Meteo/Q_C57_Daily.Time_Series_Data.2024031816461304.csv", skip=11)
+Q_DC4=read.csv("Input/Q and Meteo/Q_C57_Daily.Time_Series_Data.2024031816461304.csv", skip=11)
 #Q_DC4=read.csv("Input/Q and Meteo/Q_C57_Daily.Time_Series_Data.2024031816461304.csv", skip=11) # !!! Replace with DC2 data (analogue treatment types)
 
 Q_DC3=read.csv("Input/Q and Meteo/Q_60_daily.Time_Series_Data.2024031816502188.csv", skip=11)
 #Q_DC1=read.csv("Input/Q and Meteo/Q_60_daily.Time_Series_Data.2024031816502188.csv", skip=11) # !!! Replace with DC3 (analogue treatment types)
 
-range (Q_DC2$Q, na.rm = T)
 
 #Data Source: SITES data Portal (retreived in April 2024)
 #Q units are L s1 (from Sites Data Portal)
@@ -33,7 +32,7 @@ range (Q_DC2$Q, na.rm = T)
 
 #Format Dates
 #Q_DC4$Date=as.Date(Q_DC4$TimeStamp)
-Q_DC2$Date=as.Date(Q_DC2$TimeStamp)
+Q_DC4$Date=as.Date(Q_DC4$TimeStamp)
 Q_DC3$Date=as.Date(Q_DC3$TimeStamp)
 #Q_DC1$Date=as.Date(Q_DC1$TimeStamp)
 
@@ -46,7 +45,7 @@ Q_DC3$Date=as.Date(Q_DC3$TimeStamp)
 #Convert all Q measurements from L/s to mm3/d
 ## Remove all conversion and assume that Q data is already in m3/d (DC3 range from 0.0001 to 47, which makes sense)
 #Q_DC4$Q_m3d=Q_DC4$Q #/1000*60*60*24
-Q_DC2$Q_m3d=Q_DC2$Q /1000*60*60*24
+Q_DC4$Q_m3d=Q_DC4$Q /1000*60*60*24
 Q_DC3$Q_m3d=Q_DC3$Q /1000*60*60*24
 #Q_DC1$Q_m3d=Q_DC1$Q #/1000*60*60*24
 
@@ -58,9 +57,9 @@ Q_DC3$Q_m3d=Q_DC3$Q /1000*60*60*24
 
 #Catchment Area in m2 (originally in ha)
     #DC1_Area_m2=6.7*10000
-DC2_Area_m2=4.4*10000
+DC4_Area_m2=10.7*10000
 DC3_Area_m2=8.4*10000
-#DC4_Area_m2=10.7*10000
+#DC2_Area_m2=4.4*10000
 #DC1_Area_m2=8.4*10000 # !!!!! Same at DC3
 
         #C2_Area_m2=12*10000
@@ -71,7 +70,7 @@ DC3_Area_m2=8.4*10000
 
 #Convert all Q measurements to Specific Discharge m/d
 #Q_DC4$q_md=Q_DC4$Q_m3d/DC4_Area_m2
-Q_DC2$q_md=Q_DC2$Q_m3d/DC2_Area_m2
+Q_DC4$q_md=Q_DC4$Q_m3d/DC4_Area_m2
 Q_DC3$q_md=Q_DC3$Q_m3d/DC3_Area_m2
 #Q_DC1$q_md=Q_DC1$Q_m3d/DC1_Area_m2
 
@@ -94,27 +93,27 @@ Meteo= Meteo %>% # Remove dates in 2019, and 2023
 
 
 # Combine meteo data to Q_DC
-DC2_Q_Meteo=left_join(Meteo, Q_DC2, by = 'Date', suffix = c( "_Meteo", "_Q"))
-DC4_Q_Meteo=DC2_Q_Meteo  # Assume DC4 site Q is like DC2
+DC4_Q_Meteo=left_join(Meteo, Q_DC4, by = 'Date', suffix = c( "_Meteo", "_Q"))
+DC4_Q_Meteo=DC4_Q_Meteo  # Assume DC4 site Q is like DC4
 DC3_Q_Meteo=left_join(Meteo, Q_DC3, by = 'Date', suffix = c( "_Meteo", "_Q"))
 DC1_Q_Meteo=DC3_Q_Meteo # Assume DC1 site Q is like DC3
 
 
 # Add a column to identify the site
 DC4_Q_Meteo$Site_id=rep("DC4", nrow(DC4_Q_Meteo))
-DC2_Q_Meteo$Site_id=rep("DC2", nrow(DC2_Q_Meteo))
+DC4_Q_Meteo$Site_id=rep("DC4", nrow(DC4_Q_Meteo))
 DC3_Q_Meteo$Site_id=rep("DC3", nrow(DC3_Q_Meteo))
 DC1_Q_Meteo$Site_id=rep("DC1", nrow(DC1_Q_Meteo))
 
 
           #DC_all_Q_Meteo=rbind(DC4_Q_Meteo, 
-          #           DC2_Q_Meteo, 
+          #           DC4_Q_Meteo, 
           #          DC3_Q_Meteo,
           #           DC1_Q_Meteo)
 
 
 # Timeseries of Q in all sites _______________________________________________________________________
-ggplot(data=rbind(DC2_Q_Meteo, DC3_Q_Meteo), aes(x=Date, y=Q_m3d, color=Site_id))+
+ggplot(data=rbind(DC4_Q_Meteo, DC3_Q_Meteo), aes(x=Date, y=Q_m3d, color=Site_id))+
   geom_point()+
   #scale_color_manual(values=c(site_colors))+
   scale_x_date(limits= c(as.Date("2020-01-01"), as.Date("2022-12-31")))+
@@ -134,8 +133,13 @@ ggplot(data=rbind(DC2_Q_Meteo, DC3_Q_Meteo), aes(x=Date, y=Q_m3d, color=Site_id)
 
 library(zoo)
 
-DC4_Q_Meteo$q_md_filled= ifelse(is.na(DC2_Q_Meteo$q_md),DC3_Q_Meteo$q_md, DC2_Q_Meteo$q_md ) # When DC2_qmd is missing, use DC3_qmd
-DC2_Q_Meteo$q_md_filled= ifelse(is.na(DC2_Q_Meteo$q_md),DC3_Q_Meteo$q_md, DC2_Q_Meteo$q_md ) # When DC2_qmd is missing, use DC3_qmd
+# Assume q at DC2 is the same as DC4, but fill with DC3 when Na's
+DC4_Q_Meteo$q_md_filled= ifelse(is.na(DC4_Q_Meteo$q_md),
+                                DC3_Q_Meteo$q_md, 
+                                DC4_Q_Meteo$q_md ) # When DC4_qmd is missing, use DC3_qmd
+DC2_Q_Meteo$q_md_filled= DC4_Q_Meteo$q_md_filled  # Assume DC2 is like DC4
+
+# Assume q at DC1 is the same at DC3. 
 DC3_Q_Meteo$q_md_filled= DC3_Q_Meteo$q_md
-DC1_Q_Meteo$q_md_filled= DC1_Q_Meteo$q_md
+DC1_Q_Meteo$q_md_filled= DC3_Q_Meteo$q_md_filled
 
